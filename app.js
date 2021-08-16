2620a1aab885b940e02559b0c456f63e45cea81d
chriskhub <88444289+chriskhub@users.noreply.github.com>
2021-08-16 13:53:41 -0500

    initial attempt at  5 round game loop
    In process

--- app.js
+++ app.js
@@ -17,11 +17,9 @@ function playRound(playerSelection, computerSelection) {
     console.log(computerSelection);
     if (playerSelection == "rock" && computerSelection == "paper") {
         computerScore++;
-        checkWin(playerScore, computerScore);
         return (`You lose. Paper beats rock. The score is: ${computerScore} computer vs. ${playerScore} player.`);
     } else if (playerSelection == "rock" && computerSelection == "scissors") {
         playerScore++;
-        checkWin(playerScore, computerScore);
         return (`You win! Rock beats scissors. The score is: ${computerScore} computer vs. ${playerScore} player.`);
     } else if (playerSelection == "rock" && computerSelection == "rock") {
         return (`Same play, try again! The score is: ${computerScore} computer vs. ${playerScore} player.`);
@@ -29,21 +27,17 @@ function playRound(playerSelection, computerSelection) {
         return (`Same play, try again! The score is: ${computerScore} computer vs. ${playerScore} player.`);
     } else if (playerSelection == "paper" && computerSelection == "scissors") {
         computerScore++;
-        checkWin(playerScore, computerScore);
         return (`You lose. Scissors beat paper. The score is: ${computerScore} computer vs. ${playerScore} player.`);
     } else if (playerSelection == "paper" && computerSelection == "rock") {
         playerScore++;
-        checkWin(playerScore, computerScore);
         return (`You win! Paper beats rock. The score is: ${computerScore} computer vs. ${playerScore} player.`);
     } else if (playerSelection == "scissors" && computerSelection == "scissors") {
         return (`Same play, try again! The score is: ${computerScore} computer vs. ${playerScore} player.`);
     } else if (playerSelection == "scissors" && computerSelection == "paper") {
         playerScore++;
-        checkWin(playerScore, computerScore);
         return (`You win! Scissors beats paper. The score is: ${computerScore} computer vs. ${playerScore} player.`);
     } else if (playerSelection == "scissors" && computerSelection == "rock") {
         computerScore++;
-        checkWin(playerScore, computerScore);
         return (`You lose. Rock beats scissors. The score is: ${computerScore} computer vs. ${playerScore} player.`);
     } else {
         return ("Error! Please type rock, paper, or scissors.");
@@ -52,10 +46,21 @@ function playRound(playerSelection, computerSelection) {
 
 
 function checkWin(playerScore, computerScore) {
-    if (playerScore == 1) {
+    if (playerScore == 5) {
         console.log("You win the challenge!");
-    } else if (computerScore == 1) {
+    } else if (computerScore == 5) {
         console.log("Sorry, you lost the challenge.");
     }
 }
+
+/*Attempting to create 5 round game with a loop, but return statements from playRound function were not showing up,
+function game() {
+    for (var i = 0; i < 5; i++) {
+        playerSelection = prompt("Rock, paper, or scissors?").toLowerCase();
+        computerSelection = computerPlay().toLowerCase();
+        console.log(playerSelection);
+        console.log(computerSelection);
+        playRound(playerSelection, computerSelection);
+    }
+} */
 
