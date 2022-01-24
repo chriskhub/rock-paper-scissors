38bc56c616db8388efa9a176292beab4d156c38b
chriskhub <88444289+chriskhub@users.noreply.github.com>
2022-01-23 18:08:46 -0600

    Added checkWin function to playRound.

--- app.js
+++ app.js
@@ -17,50 +17,51 @@ function playRound(playerSelection, computerSelection) {
     console.log(computerSelection);
     if (playerSelection == "rock" && computerSelection == "paper") {
         computerScore++;
-        return (`You lose. Paper beats rock. The score is: ${computerScore} computer vs. ${playerScore} player.`);
+        console.log (`You lose. Paper beats rock. The score is: ${computerScore} computer vs. ${playerScore} player.`);
     } else if (playerSelection == "rock" && computerSelection == "scissors") {
         playerScore++;
-        return (`You win! Rock beats scissors. The score is: ${computerScore} computer vs. ${playerScore} player.`);
+        console.log (`You win! Rock beats scissors. The score is: ${computerScore} computer vs. ${playerScore} player.`);
     } else if (playerSelection == "rock" && computerSelection == "rock") {
-        return (`Same play, try again! The score is: ${computerScore} computer vs. ${playerScore} player.`);
+        console.log (`Same play, try again! The score is: ${computerScore} computer vs. ${playerScore} player.`);
     } else if (playerSelection == "paper" && computerSelection == "paper") {
-        return (`Same play, try again! The score is: ${computerScore} computer vs. ${playerScore} player.`);
+        console.log (`Same play, try again! The score is: ${computerScore} computer vs. ${playerScore} player.`);
     } else if (playerSelection == "paper" && computerSelection == "scissors") {
         computerScore++;
-        return (`You lose. Scissors beat paper. The score is: ${computerScore} computer vs. ${playerScore} player.`);
+        console.log (`You lose. Scissors beat paper. The score is: ${computerScore} computer vs. ${playerScore} player.`);
     } else if (playerSelection == "paper" && computerSelection == "rock") {
         playerScore++;
-        return (`You win! Paper beats rock. The score is: ${computerScore} computer vs. ${playerScore} player.`);
+        console.log (`You win! Paper beats rock. The score is: ${computerScore} computer vs. ${playerScore} player.`);
     } else if (playerSelection == "scissors" && computerSelection == "scissors") {
-        return (`Same play, try again! The score is: ${computerScore} computer vs. ${playerScore} player.`);
+        console.log (`Same play, try again! The score is: ${computerScore} computer vs. ${playerScore} player.`);
     } else if (playerSelection == "scissors" && computerSelection == "paper") {
         playerScore++;
-        return (`You win! Scissors beats paper. The score is: ${computerScore} computer vs. ${playerScore} player.`);
+        console.log (`You win! Scissors beats paper. The score is: ${computerScore} computer vs. ${playerScore} player.`);
     } else if (playerSelection == "scissors" && computerSelection == "rock") {
         computerScore++;
-        return (`You lose. Rock beats scissors. The score is: ${computerScore} computer vs. ${playerScore} player.`);
+        console.log (`You lose. Rock beats scissors. The score is: ${computerScore} computer vs. ${playerScore} player.`);
     } else {
-        return ("Error! Please type rock, paper, or scissors.");
+        console.log ("Error! Please type rock, paper, or scissors.");
     }
+    checkWin(playerScore, computerScore);
 }
 
 
 function checkWin(playerScore, computerScore) {
-    if (playerScore == 5) {
+    if (playerScore === 3) {
         console.log("You win the challenge!");
-    } else if (computerScore == 5) {
+    } else if (computerScore === 3) {
         console.log("Sorry, you lost the challenge.");
     }
 }
 
-/*Attempting to create 5 round game with a loop, but return statements from playRound function were not showing up,
-function game() {
-    for (var i = 0; i < 5; i++) {
-        playerSelection = prompt("Rock, paper, or scissors?").toLowerCase();
-        computerSelection = computerPlay().toLowerCase();
-        console.log(playerSelection);
-        console.log(computerSelection);
-        playRound(playerSelection, computerSelection);
-    }
-} */
+// function game() {
+//     for (var i = 0; i < 5; i++) {
+//         playerSelection = prompt("Rock, paper, or scissors?").toLowerCase();
+//         computerSelection = computerPlay().toLowerCase();
+//         checkWin(playerScore, computerScore);
+//         console.log(playerSelection);
+//         console.log(computerSelection);
+//         playRound(playerSelection, computerSelection);
+//     }
+// }
 
